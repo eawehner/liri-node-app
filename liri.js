9b8779a13664c8beb3cf0a7890776a4381c7a066
EAWehner <elizabethawehner@gmail.com>
2018-11-09 20:09:02 -0500

    functionality added for all functions

--- liri.js
+++ liri.js
@@ -3,42 +3,42 @@ require("dotenv").config();
 
 //requiring everything we need
 var request = require("request");
+var fs = require("fs");
 var Spotify = require('node-spotify-api');
 
 //accessing spotify keys
 var keys = require("./key");
 
 var spotify = new Spotify(keys.spotify);
-// console.log(spotify.credentials.id + "\n" + spotify.credentials.secret);
-// console.log(spotify);
 
-//SET UP OUR PROCESS.ARGV STUFF HERE, [2] for command and [3+] for search
-//will need to create a loop through the process.argv array to put together the search
+//setting up our command variable
 var command = process.argv[2];
-var search = process.argv[3];
+
+//constructing our search variable
+var searchTerm = process.argv.splice(3);
+var search = searchTerm.join("+");
 
 //CONCERT CODE
 //command: 'concert-this' [user searches artist or band name here, set to artist var]
-//pull data from "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
 
-if (command === "concert-this") {
+function concertThis(search) {
     //request URL for Bands In Town
     var requestURL = "https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp";
 
     request(requestURL, function(error, response, body) {
         if (!error && response.statusCode === 200) {
             console.log("Top 5 Venues Available:");
-
+    
             var data = JSON.parse(body);
-
+    
             for (i=0; i<5; i++) {
                 console.log("\n-----\n");
                 console.log("Event Location: " + data[i].venue.name);
                 console.log("Country: " + data[i].venue.country);
                 console.log("City: " + data[i].venue.city + ", " + data[i].venue.region);
-
+    
                 var date = new Date(data[i].datetime);
-
+    
                 console.log("Time: " + date);
                 console.log("\n-----\n");
             };
@@ -46,15 +46,14 @@ if (command === "concert-this") {
     });
 };
 
+
 //SPOTIFY CODE
 //command: 'spotify-this-song' [user searches song name here, var set to song]
-//use spotify API
 
-if (command === "spotify-this-song") {
+function spotifyThis(search) {
     spotify
     .search({type: "track", query: search, limit: 1})
     .then(function(response) {
-        // console.log(response);
         var data = response.tracks.items[0];
 
         console.log("\n-----\n");
@@ -67,14 +66,12 @@ if (command === "spotify-this-song") {
     .catch(function(error) {
         console.log(error);
     });
-
 };
 
 //MOVIE CODE
 //command: 'movie-this' [user searches movie title here]
-//pulls from IMBD
 
-if (command === "movie-this") {
+function movieThis(search) {
     //request URL for the OMDB API
     var requestURL = "http://www.omdbapi.com/?apikey=trilogy&t=" + search;
 
@@ -124,10 +121,53 @@ if (command === "movie-this") {
     };
 };
 
+
+//running the checks for commands
+
+if (command === "concert-this") {
+    concertThis(search);
+};
+
+if (command === "spotify-this-song") {
+    spotifyThis(search);
+};
+
+//MOVIE CODE
+//command: 'movie-this' [user searches movie title here]
+
+if (command === "movie-this") {
+    movieThis(search);
+};
+
 //DO WHAT IT SAYS
 //command 'do-what-it-says'
 //pull from random.txt to use the command written there
 
 if (command === "do-what-it-says") {
+
+    fs.readFile("random.txt", "utf-8", function(error, data) {
+        if (error) {
+            console.log("Something went wrong...");
+            console.log(error);
+        } else {
+            var randomTxtArr = data.split(",");
+            var doCommand = randomTxtArr[0];
+            var doSearchTerm = randomTxtArr.splice(1);
+            var doSearch = doSearchTerm.join("+");
+
+            console.log("Running command from random.txt...");
+            console.log(doCommand + " " + doSearch);
+
+            if (doCommand === "concert-this") {
+                concertThis(doSearch);
+            } else if (doCommand === "spotify-this-song") {
+                spotifyThis(doSearch);
+            } else if (doCommand === "movie-this") {
+                movieThis(doSearch);
+            } else {
+                console.log("There is nothing we can do with random.txt.");
+            }
+        };
+    });
 
 };
